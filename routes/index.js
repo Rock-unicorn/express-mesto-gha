b1125466e650e6b8906a58c90d9ad88eee89092f
Dmitry Tsygankov <rockcorn@yandex.ru>
2023-05-01 14:41:29 +0300

    13PR 1st init: correction of typos

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ const { ERROR_CODE_NOT_FOUND } = require('../utils/errors');
 
 router.use('/', usersRouter);
 router.use('/', cardsRouter);
-router.use('*', (req, res) => req.statusCode(ERROR_CODE_NOT_FOUND).send({ message: 'Данные по запросу не найдены' }));
+router.use('*', (req, res) => res.statusCode(ERROR_CODE_NOT_FOUND).send({ message: 'Данные по запросу не найдены' }));
 
 module.exports = router;
