a506ff1aded35875f535884cb1f1af19f56d8a37
Dmitry Tsygankov <rockcorn@yandex.ru>
2023-05-09 18:14:52 +0300

    14PR: refactoring1

--- controllers/cards.js
+++ controllers/cards.js
@@ -32,7 +32,7 @@ const deleteCard = (req, res, next) => {
       if (req.user._id !== card.owner.toString()) {
         next(new ForbiddenError('Пользователи не могут удалять чужие карточки'));
       }
-      return card.remove().then(() => res.send({ data: card }));
+      return card.remove().then(() => res.send(card));
     })
     .catch((err) => {
       if (err.name === 'CastError') {
@@ -44,6 +44,7 @@ const deleteCard = (req, res, next) => {
 
 const likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .orFail()
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
@@ -58,6 +59,7 @@ const likeCard = (req, res, next) => {
 
 const dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .orFail()
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'DocumentNotFoundError') {
