db73971e0f6db181eca816b0fc1ab02e7fa9fff5
Dmitry Tsygankov <rockcorn@yandex.ru>
2023-05-23 10:46:29 +0300

    logger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const middlewareErrors = require('./utils/errors/middleware-errors');
 const { validationLogin, validationCreateUser } = require('./middlewares/validation');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
@@ -15,10 +16,12 @@ const app = express();
 app.use(express.json());
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(helmet());
+app.use(requestLogger);
 app.post('/signin', validationLogin, login);
 app.post('/signup', validationCreateUser, createUser);
 app.use(auth);
 app.use('/', routes);
+app.use(errorLogger);
 app.use(errors());
 app.use(middlewareErrors);
 
